822c02153dd6e77d25604106459ff8a84052b1ea
Craig Patten <cjp@cjp.id.au>
2015-03-18 02:04:50 +1030

    +kafka

---
+++ sub/kafka/kafka.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+if [ -z "$CLUSTER" ] ; then
+  echo "required environment: CLUSTER; skipping kafka"
+  exit 0
+fi
+
+zfs create data/kafka
+
+export KAFKA_BROKER=`echo $CLUSTER | xargs -n 1 | cat -n | grep $HOST | awk '{print $1}'`
+
+../../render.py < server.properties > /opt/kafka/config/server.properties
+
+chown -RH ubuntu:ubuntu /{opt,data}/kafka
