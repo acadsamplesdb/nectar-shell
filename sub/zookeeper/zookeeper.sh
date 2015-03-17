2f69dca87473a2240a4ea59aa4f3fc3796d6d2d3
Craig Patten <cjp@cjp.id.au>
2015-03-17 17:12:24 +1030

    +zookeeper

---
+++ sub/zookeeper/zookeeper.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+if [ -z "$ZOOKEEPER_ID" -o -z "$CLUSTER" ] ; then
+  echo "required environment: ZOOKEEPER_ID CLUSTER; skipping zookeeper"
+  exit 0
+fi
+
+zfs create data/zookeeper
+
+../../render.py < zoo.cfg > /opt/zookeeper/conf/zoo.cfg
+
+echo $CLUSTER | xargs -n 1 | cat -n | grep $HOST | awk '{print $1}' > /data/zookeeper/myid
+
+chown -RH ubuntu:ubuntu /{opt,data}/zookeeper
