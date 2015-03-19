a4ec5fcd08b11130a93657577a4ea591e577c03c
Craig Patten <cjp@cjp.id.au>
2015-03-19 16:41:16 +1030

    supervisor workaround

--- sub/misc/misc.sh
+++ sub/misc/misc.sh
@@ -19,3 +19,8 @@ install -o 0 -g 0 ntp.conf /etc
 # open the console for output
 
 chmod a+w /dev/console
+
+# supervisor workaround for init script being ignored on ubuntu 14.10
+# (no points for elegance)
+
+echo "service supervisor start" >> /etc/rc.local
