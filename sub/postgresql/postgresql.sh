040227980c89f3401c399346e85fc4c3aa2dcfd4
Craig Patten <cjp@cjp.id.au>
2015-12-04 12:42:43 +1030

    +postgresql

---
+++ sub/postgresql/postgresql.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# packages
+
+apt-get update
+apt-get -y install postgresql
+
+# ubuntu user
+
+su -l postgres -c 'createuser --superuser ubuntu'
+
+# zfs
+
+service postgresql stop
+
+fs=data/postgresql
+zfs create $fs
+chown postgres:postgres /$fs
+mv /var/lib/postgresql/* /$fs
+rmdir /var/lib/postgresql
+ln -s /$fs /var/lib/postgresql
+
+# auth
+
+cp -f pg_hba.conf /etc/postgresql/9.4/main
+
+# done
+
+service postgresql start
